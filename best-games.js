3a7e90d9e38d4f5030db200f6ec260673c80141b
Noah <hanlun.song@gmail.com>
2021-06-24 21:35:28 -0400

    complete terminal app

--- best-games.js
+++ best-games.js
@@ -1,21 +1,73 @@
-const userReviews = require("./user-reviews.js");
-const mediaReviews = require("./media-reviews.js");
+const puppeteer = require("puppeteer");
+const inquirer = require("inquirer");
 
-const bestGames = [];
-userReviews.forEach((userReview) => {
-  mediaReviews.forEach((mediaReview) => {
-    if (
-      mediaReview.title === userReview.title &&
-      mediaReview.score >= 85 &&
-      userReview.score >= 8.5
-    ) {
-      bestGames.push({
-        title: userReview.title,
-        user_score: userReview.score,
-        media_score: mediaReview.score,
-      });
-    }
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "platform",
+      message: "Which platform are you interested in?",
+      choices: [
+        "ps4",
+        "ps5",
+        "xboxone",
+        "xbox-series-x",
+        "switch",
+        "pc",
+        "ios",
+        "stadia",
+      ],
+    },
+  ])
+  .then((answers) => {
+    const platform = answers.platform;
+    const urls = [
+      `https://www.metacritic.com/browse/games/release-date/available/${platform}/userscore`,
+      `https://www.metacritic.com/browse/games/release-date/available/${platform}/metascore`,
+    ];
+    getGames(urls);
   });
-});
 
-console.table(bestGames);
+async function getGames(urls) {
+  const browser = await puppeteer.launch({ headless: false });
+
+  const collect = async (url) => {
+    const page = await browser.newPage();
+    await page.goto(url);
+    return await page.evaluate(() => {
+      let allWraps = document.querySelectorAll(".clamp-summary-wrap");
+      let reviews = [];
+      allWraps.forEach((wrap) => {
+        const title = wrap.querySelector(".title h3");
+        const score = wrap.querySelector(".metascore_anchor div");
+        reviews.push({
+          title: title.textContent,
+          score: score.textContent,
+        });
+      });
+      return reviews;
+    });
+  };
+
+  const allReviews = await Promise.all(urls.map(collect));
+
+  const bestGames = [];
+  allReviews[0].forEach((review0) => {
+    allReviews[1].forEach((review1) => {
+      if (
+        review1.title === review0.title &&
+        review1.score >= 85 &&
+        review0.score >= 8.5
+      ) {
+        bestGames.push({
+          title: review0.title,
+          user_score: review0.score,
+          media_score: review1.score,
+        });
+      }
+    });
+  });
+
+  console.table(bestGames);
+  process.exit(1);
+}
