632bd0717e41b76e816e47e7b6201e8322204f7c
Noah <hanlun.song@gmail.com>
2021-06-27 20:13:41 -0400

    turn it into PWA

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,57 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="A smart table to help you filter and find games that score high on metacritic."
+        />
+        <meta name="keywords" content="metacritic, video game, review, game" />
+        <meta name="theme-color" content="#317EFB" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-starturl" content="/" />
+
+        <title>Best Metacritic Games</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/icons/android-chrome-512x512.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="180x180"
+          href="/icon.ong"
+        />
+
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 
@@ -67,11 +66,6 @@ export default function Home() {
 
   return (
     <div className="min-h-screen">
-      <Head>
-        <title>Best Metacritic Games</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <div className="bg-gray-800">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
           <div className="max-w-2xl mb-10">
